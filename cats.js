64b1f1117044977c6c302892abba69ad15882c58
Daniel Valle <daniel.vallecl@gmail.com>
2016-07-08 15:31:31 -0400

    Added option to retrieve a different number of cats.

--- cats.js
+++ cats.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
     $.ajax({
       url: "http://bitkittens.herokuapp.com/cats.json",
       method: "GET",
-      data: {},
+      data: {number: 5},
       dataType: 'json'
     }).done(function(responseData) {
-                                                      //You must add document in front when creating an Element//
+
+      //Iterating through the server's response (responseData) to get individual cats//
       for (i = 0; i < responseData["cats"].length; i++) {
-        var image = document.createElement("IMG");
+        var image = document.createElement("IMG");     //You must add document in front when creating an Element//
         $(image).attr("src", responseData["cats"][i]["photo"]);
         $("#cat" + (i + 1)).append(image);
       }
